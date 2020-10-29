6db77a2aff30e6b127d402b0f1cb5aa7f2aaf6a3
Mary-Ann Horley <mh@penguin>
2020-10-29 08:36:22 +0000

    updated config code

--- config.py
+++ config.py
@@ -1,21 +1,12 @@
 import os
 
 TRELLO_KEY = os.environ.get("TRELLO_KEY")
-
 TRELLO_TOKEN = os.environ.get("TRELLO_TOKEN")
-
 BOARD_ID = os.environ.get("BOARD_ID")
-
 POSTS_LABEL = os.environ.get("POSTS_LABEL")
-
 PAGES_LABEL = os.environ.get("PAGES_LABEL")
-
 DOWNLOADS_LABEL = os.environ.get("DOWNLOADS_LABEL")
-
 LINKS_LABEL = os.environ.get("LINKS_LABEL")
-
 BASE_URL = os.environ.get("BASE_URL")
-
 MONGODB_USER = os.environ.get("MONGODB_USER")
-
 MONGODB_PASS = os.environ.get("MONGODB_PASS")
