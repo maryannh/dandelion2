fe5b7689355992ea2d9fd68fab65834650b1e216
Mary-Ann Horley <mh@penguin>
2020-10-30 11:32:47 +0000

    updated db schema and added page

--- app.py
+++ app.py
@@ -76,5 +76,5 @@ def download(page_id):
     text = markdown(info["text"])
     return render_template("page.html", info=info, text=text)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+# if __name__ == '__main__':
+    # app.run(debug=True, host='0.0.0.0')
