aac1203e08f5f96cdf86d1a15f23929cd708511d
maryannh <maryann.horley@gmail.com>
2020-12-29 07:21:58 +0000

    Updated index template

--- app.py
+++ app.py
@@ -15,84 +15,27 @@ app.config.from_pyfile('config.py', silent=True)
 
 markdown = mistune.Markdown()
 
-def content_stats(page_type, item_id, info):
+def get_subjects():
     db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
-    db.stats.insert_one({
-      "datetime": datetime.datetime.now(),
-      "page": info["title"],
-      "type": page_type,
-      "item_id": item_id,
-      "published": info["date"],   
-      "tags": info["tags"],
-      "subjects": info["subjects"],
-      "referrer": request.headers.get("Referer"),
-      "string": request.user_agent.string,
-    })
-
-def cat_stats(page_type, page_name):
-    db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
-    db.stats.insert_one({
-      "datetime": datetime.datetime.now(),
-      "page": page_name,
-      "type": page_type,
-      "referrer": request.headers.get("Referer"),
-      "string": request.user_agent.string,
-    })
-
-@app.errorhandler(404)
-def page_not_found(e):
-    cat_stats("error", "404")
-    return render_template('404.html'), 404
-
-@app.route("/")
-def index():
-    db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
-    post_loop = list(db.content.find({
-        "type": "post"
-    }).sort("date", -1))
-    page_loop = list(db.content.find({
-        "type": "page"
-    }))
-    download_loop = list(db.content.find({
-        "type": "download"
-    }).sort("date", -1))
-    link_loop = list(db.content.find({
-        "type": "link"
-    }).sort("date", -1))
-
-    return render_template("index.html", post_loop=post_loop, page_loop=page_loop,
-        download_loop=download_loop, link_loop=link_loop)
-
-@app.route("/subjects")
-def subjects():
-    db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
-
     subject_list = list(db.subjects.find())
-    print(subject_list)
-
     subjects = []
-    
     for subject in subject_list:
         slug = subject["slug"]
         name = subject["name"]
         count = db.content.count({
-          "subjects": slug
-          })
+        "subjects": slug
+            })
         if int(count) > 2:
-          info = {
-            "slug": slug,
-            "name": name,
-            "count": count,
-          }
-          subjects.append(info)
-    print(subjects)
+            info = {
+              "slug": slug,
+              "name": name,
+              "count": count,
+            }
+        subjects.append(info)
+    return subjects
 
-    return render_template("subjects.html", subjects=subjects)
-
-@app.route("/tags")
-def tags():
+def get_tags():
     db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
-
     tag_list = list(db.tags.find())
 
     tags = []
@@ -110,12 +53,47 @@ def tags():
             "count": count,
           }
           tags.append(info)
+    return tags
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/")
+def index():
+    db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
+    post_loop = list(db.content.find({
+        "type": "post"
+    }).sort("date", -1))
+    page_loop = list(db.content.find({
+        "type": "page"
+    }))
+    download_loop = list(db.content.find({
+        "type": "download"
+    }).sort("date", -1))
+    link_loop = list(db.content.find({
+        "type": "link"
+    }).sort("date", -1))
+    subjects = get_subjects()
+    tags = get_tags()
+    return render_template("index.html", post_loop=post_loop, page_loop=page_loop,
+        download_loop=download_loop, link_loop=link_loop, subjects=subjects, tags=tags)
+
+@app.route("/subjects")
+def subjects():
+    subjects = get_subjects()
+    return render_template("subjects.html", subjects=subjects)
+
+@app.route("/tags")
+def tags():
+    db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
+
+    tags = get_tags()
 
     return render_template("tags.html", tags=tags)
 
 @app.route("/about")
 def about():
-    cat_stats("page", "about")
     return render_template("about.html")
 
 @app.route("/cron")
@@ -138,7 +116,6 @@ def why():
         "type": "link",
         "tags": {"$in": ["why_and_how"] }
     }).sort("date", -1))
-    cat_stats("tag", "why_and_how")
     return render_template("why.html", links=links, posts=posts)
 
 @app.route("/tag/<tag>")
@@ -172,8 +149,6 @@ def tag(tag):
     info = db.tags.find_one({
         "slug": tag,
     })
-
-    cat_stats("tag", tag)
 
     return render_template("tag.html", links=links, posts=posts, tag=tag, downloads=downloads, info=info)
 
@@ -211,8 +186,6 @@ def subject(subject):
         "slug": subject,
     })
 
-    cat_stats("subject", subject)
-
     return render_template("subject.html", links=links, posts=posts, tag=tag, downloads=downloads, info=info)
 
 @app.route("/post/<post_id>")
@@ -220,7 +193,6 @@ def post(post_id):
     db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
     info = db.content.find_one({"item_id": post_id})
     text = markdown(info["text"])
-    content_stats("post", post_id, info)
     return render_template("post.html", info=info, text=text)
 
 @app.route("/page/<page_id>")
@@ -228,7 +200,6 @@ def page(page_id):
     db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
     info = db.content.find_one({"item_id": page_id})
     text = markdown(info["text"])
-    content_stats("page", page_id, info)
     return render_template("page.html", info=info, text=text)
 
 @app.route("/download/<page_id>")
@@ -236,7 +207,6 @@ def download(page_id):
     db = MongoClient("mongodb+srv://admin:" + config.MONGODB_PASS + "@cluster0.mfakh.mongodb.net/blog?retryWrites=true&w=majority", connect=False).blog
     info = db.content.find_one({"item_id": page_id})
     text = markdown(info["text"])
-    content_stats("download", page_id, info)
     return render_template("page.html", info=info, text=text)
 
 # if __name__ == '__main__':
